d546d07fb29497c004e2a531a237695370bdf3d9
Twoods196 <marketing@cybermediavision.com>
2015-09-17 13:25:49 -0500

    Update SimpleCho - Twoods196.lua

--- SimpleCho - Twoods196.lua
+++ SimpleCho - Twoods196.lua
@@ -8,13 +8,13 @@ local author = "Twoods196"
 local SCRIPT_NAME = "SimpleCho"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.githubusercontent.com"
-local UPDATE_PATH = "Twoods196/BOLSimpleCho/master/SimpleCho%20-%20Twoods196.lua".."?rand="..math.random(1,10000)
+local UPDATE_PATH = "/Twoods196/BOLSimpleCho/master/SimpleCho%20-%20Twoods196.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
 function AutoupdaterMsg(msg) print("<font color=\"#FF0000\"><b>SimpleCho:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST,"Twoods196/BOLSimpleCho/master/SimpleCho.version")
+	local ServerData = GetWebResult(UPDATE_HOST,"/Twoods196/BOLSimpleCho/master/SimpleCho.version")
 	if ServerData then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
